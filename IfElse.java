b847d42e96daf13de07f35defefb24ecc6fbbaa5
ilgiz_dzh <151811780+fewtori@users.noreply.github.com>
2023-11-25 18:51:25 +0600

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] arg) {
+        int a = 15, b = 14;
+        if (a<=b) {
+            System.out.println("Not so bad");
+        } else {
+            System.out.println("Awesome");
+        }
+    }
+}

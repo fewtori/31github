e645f75ba3f0e0797762cdf6c573bb4b7884e548
ilgiz_dzh <151811780+fewtori@users.noreply.github.com>
2023-11-25 17:50:48 +0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] arg) {
+        int name = 15;
+            System.out.println(name);
+
+                String Brand = "Range Rover";
+                System.out.println(Brand);
+                    double half = 1.5;
+                    System.out.println(half);
+    }
+}

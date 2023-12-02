80b3c05364c2fca587a3196f2a387b200a4e40e3
ilgiz_dzh <151811780+fewtori@users.noreply.github.com>
2023-12-02 23:47:59 +0600

    Add files via upload

---
+++ PrimitiveTypes.java
@@ -0,0 +1,9 @@
+public class PrimitiveTypes {
+    public static void main (String[] arg) {
+        int IphoneMemory = 128;
+        byte CardMemory = 64;
+        char Mark = 5;
+        System.out.println(IphoneMemory + CardMemory + Mark);
+    }
+
+}

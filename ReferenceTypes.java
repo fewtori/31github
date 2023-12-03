f991444e67f1c8952f785532f7f46729c508a12e
ilgiz_dzh <151811780+fewtori@users.noreply.github.com>
2023-12-03 23:55:18 +0600

    Add files via upload

---
+++ ReferenceTypes.java
@@ -0,0 +1,11 @@
+public class ReferenceTypes {
+    package codeWithMosh;
+    import java.util.Date;
+    public static void main(String[] arg ) {
+        int a = 30
+
+        Date now = new Date();
+        System.out.println(Date);
+
+    }
+}

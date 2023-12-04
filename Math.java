2d4662d611582f940c430da17fc3c5c512681080
ilgiz_dzh <151811780+fewtori@users.noreply.github.com>
2023-12-04 23:54:28 +0600

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Math {
+    public static void main(String[] args) {
+        int a = 74;
+        byte b = 36;
+        System.out.println(a+b);
+        System.out.println(74+36);
+            int c = 50;
+                    int d = 3;
+        System.out.println(c/d);
+        }
+    }
